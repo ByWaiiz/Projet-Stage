25843b9ba838ac4c7ea0602318e269443902fd92
bywaiiz <craftd571@gmail.com>
2023-05-27 18:07:58 +0200

    main

--- index.php
+++ index.php
@@ -10,12 +10,13 @@
 <div class="container">
     <div class="content">
         <div class="login">
+            <h3>Se connecter</h3>
             <form method="POST">
                 <label for="username">Nom d'utilisateur : </label>
                 <input type="text" name="username" id="username" placeholder="Nom d'utilisateur">
                 <label for="password">Mot de passe : </label>
                 <input type="password" name="password" id="password" placeholder="Mot de passe">
-                <input type="submit" name="login" id="login" value="Login">
+                <input type="submit" name="login" id="login" value="Se connecter">
             </form>
             <a href="">Inscription</a>
         </div>
